913b1fcd8bf2ec529cf6c48a2b7ba855cfc6c0b6
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-08-10 23:59:50 +0300

    add imageCloudApi in env for remote image storage; images no longer stored on the backend server, but on cloudinary

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   // urlBackend: 'https://ecommerce-backend-zclv.onrender.com',
   urlBackend: 'https://ecommerce-backend-sage-iota.vercel.app',
+  imageCloudApi: 'https://res.cloudinary.com/dr5b9dgkf/image/upload/ecommerce-images',
   // urlBackend: 'http://localhost:3002',
 
   publishableKey: 'pk_test_51NDlSYILKyCEUwa5feaOGR6njXoZKTCdNeBs3LqDt7JwJcNCtzMwhWnRWdNmVTF6bciDbvMaMe3kBwmjN8PXxkx500HcKZ8CB5',
