f593ed9354d245f6e19f7900b7cc72245f6a8fc1
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-08-10 23:21:55 +0300

    changed backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // urlBackend: 'https://ecommerce-backend-zclv.onrender.com',
-  urlBackend: 'https://ecommerce-backend-rj5sjkavm-adrianilie01s-projects.vercel.app',
+  urlBackend: 'https://ecommerce-backend-sage-iota.vercel.app',
   // urlBackend: 'http://localhost:3002',
 
   publishableKey: 'pk_test_51NDlSYILKyCEUwa5feaOGR6njXoZKTCdNeBs3LqDt7JwJcNCtzMwhWnRWdNmVTF6bciDbvMaMe3kBwmjN8PXxkx500HcKZ8CB5',
