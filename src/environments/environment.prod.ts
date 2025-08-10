59c11bc6076d4d964ad7e1aaec5e4cad3907dccb
AdrianIlie01 <ilieadrian01@yahoo.com>
2025-08-11 01:47:37 +0300

    update backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // urlBackend: 'https://ecommerce-backend-zclv.onrender.com',
-  urlBackend: 'https://ecommerce-backend-adrianilie01s-projects.vercel.app',
+  urlBackend: 'https://ecommerce-backend-psi-gold.vercel.app',
   imageCloudApi: 'https://res.cloudinary.com/dr5b9dgkf/image/upload/ecommerce-images',
   // urlBackend: 'http://localhost:3002',
 
